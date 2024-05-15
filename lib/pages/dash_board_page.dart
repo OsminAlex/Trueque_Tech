23cab561fc7d19c4dc3f6ca81af4e73b188c5b84
Ysvage <yassersaldana25@gmail.com>
2024-05-15 16:24:44 -0600

    una cagada todo

--- lib/pages/dash_board_page.dart
+++ lib/pages/dash_board_page.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:trueque_tech/pages/favorite_page.dart';
+import 'package:trueque_tech/pages/publish_page.dart';
+import 'package:trueque_tech/themes/colors.dart';
+import 'package:trueque_tech/pages/profile_page.dart';
+import 'package:trueque_tech/pages/chat_page.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class DashBoardPage extends StatelessWidget {
-  static const String routename = 'DashBoard';
-  const DashBoardPage({super.key});
+class DashBoardPage extends StatefulWidget {
+  static const String routename = 'Dashboard';
+  const DashBoardPage({Key? key}) : super(key: key);
+
+  @override
+  _DashBoardPageState createState() => _DashBoardPageState();
+}
+
+class _DashBoardPageState extends State<DashBoardPage> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return Scaffold(
+      bottomNavigationBar: GNav(
+        selectedIndex: _selectedIndex,
+        onTabChange: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          _navigateToPage(context, index);
+        },
+        backgroundColor: AppColors.sinDefinir04.withOpacity(0.2),
+        iconSize: 25,
+        gap: 8,
+        padding: EdgeInsets.all(18),
+        tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+          ),
+          GButton(
+            icon: Icons.favorite,
+            text: 'Favorite',
+          ),
+          GButton(
+            icon: Icons.add_box_outlined,
+            text: 'Publish',
+          ),
+          GButton(
+            icon: Icons.forum,
+            text: 'Chat',
+          ),
+          GButton(
+            icon: Icons.person,
+            text: 'Profile',
+          ),
+        ],
       ),
     );
+  }
+
+  void _navigateToPage(BuildContext context, int index) {
+    switch (index) {
+      /*case 1:
+        Navigator.pushNamed(context, FavoritePage.routename);
+        break;
+      case 2:
+        Navigator.pushNamed(context, PublishPage.routename);
+        break;
+      case 3:
+        Navigator.pushNamed(context, ChatPage.routename);
+        break;*/
+      case 4:
+        Navigator.pushNamed(context, ProfilePage.routename);
+        break;
+    }
   }
 }
