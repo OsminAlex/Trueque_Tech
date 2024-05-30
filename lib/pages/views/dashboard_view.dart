9162490effe74e82aca4af7b69621288069f3ea0
Osmin Mendoza <osmin.mendozasevilla@gmail.com>
2024-05-29 19:05:30 -0600

    Scrollview test

--- lib/pages/views/dashboard_view.dart
+++ lib/pages/views/dashboard_view.dart
@@ -12,58 +12,264 @@ class DashboardView extends StatelessWidget {
             backgroundColor: Colors.white,
             body: Padding(
               padding: EdgeInsets.symmetric(vertical: 3.h, horizontal: 1.5.h),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Stack(
                 children: [
-                  TextField(
-                    style: TextStyle(color: AppColors.BotonesApp),
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey[50],
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide.none,
-                        ),
-                        hintText: AppStrings.searchbarHint,
-                        hintStyle: TextStyle(
-                            //fontSize: 12.sp,
-                            color: AppColors.BotonesApp.withOpacity(0.3)),
-                        prefixIcon: Icon(Icons.search),
-                        prefixIconColor: AppColors.BotonesApp),
-                  ),
-                  SizedBox(
-                    height: 3.h,
-                  ),
-                  Text(
-                    AppStrings.dashboardNews,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 3.h,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          height: 100,
-                          width: 300,
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                              color: Colors.deepPurple.shade400,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Center(
-                            child: Text("Subs"),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16.h),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
                           ),
-                        ),
-                      ],
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 100,
+                            width: 300,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurple.shade400,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Text("Subs"),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          ),
+                        ],
+                      ),
                     ),
-                  )
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        style: TextStyle(color: AppColors.BotonesApp),
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[50],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: AppStrings.searchbarHint,
+                            hintStyle: TextStyle(
+                                //fontSize: 12.sp,
+                                color: AppColors.BotonesApp.withOpacity(0.3)),
+                            prefixIcon: Icon(Icons.search),
+                            prefixIconColor: AppColors.BotonesApp),
+                      ),
+                      SizedBox(
+                        height: 3.h,
+                      ),
+                      Text(
+                        AppStrings.dashboardNews,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 3.h,
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ));
